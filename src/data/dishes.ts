0cd490ef427f2a43d152a6e659b46ce83ac4a52c
commander-error404 <python3.go.c@gmail.com>
2026-03-11 08:31:17 -0700

    add all Plats principaux

--- src/data/dishes.ts
+++ src/data/dishes.ts
@@ -8,27 +8,117 @@ export const dishes: Dish[] = [
     image: 'NGALAM-TRAITEUR-IMG-20230717-WA0056-jpg.jpg',
   },
   {
-    id: 1,
+    id: 2,
     name: '',
     category: 'Entrées et salades',
     image: 'NGALAM-TRAITEUR-20230805-220219-jpg.jpg',
   },
   {
-    id: 1,
+    id: 3,
     name: 'Saumon avocat',
     category: 'Entrées et salades',
     image: 'NGALAM-TRAITEUR-20230805-202716-jpg.jpg',
   },
   {
-    id: 1,
+    id: 4,
     name: 'Ballotine de poulet farcie aux champignons ',
     category: 'Entrées et salades',
     image: 'NGALAM-TRAITEUR-20230902-201013-jpg.jpg',
   },
   {
-    id: 1,
+    id: 5,
     name: 'Variété mezzé libanais',
     category: 'Entrées et salades',
     image: 'NGALAM-TRAITEUR-20230805-222818-jpg.jpg',
   },
+
+  {
+    id: 6,
+    name: 'Effiloché de boeuf',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-20230617-150518-2-jpg.jpg',
+  },
+  {
+    id: 7,
+    name: 'Pave de saumon',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-20230610-212320-jpg.jpg',
+  },
+
+  {
+    id: 8,
+    name: 'Suprême de poulet',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-20230902-201013-jpg.jpg',
+  },
+
+  {
+    id: 9,
+    name: 'Cuisse de canard confite',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-IMG-20230714-WA0021-jpg.jpg',
+  },
+
+  {
+    id: 10,
+    name: 'Souris d’agneau',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-IMG-20230717-WA0055-jpg.jpg',
+  },
+
+  {
+    id: 11,
+    name: 'Boeuf bourguignon, et ses legumes de saison',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-20230902-190002-jpg.jpg',
+  }, 
+  {
+    id: 12,
+    name: 'Variété mzzé libanais',
+    category: 'Entrées et salades',
+    image: 'NGALAM-TRAITEUR-20230805-222818-jpg.jpg',
+  },
+  
+  {
+    id: 13,
+    name: 'Poulet coco curry',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-20230902-201013-jpg.jpg',
+  },
+  {
+    id: 15,
+    name: '',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-IMG-20230714-WA0035-jpg.jpg',
+  },
+  {
+    id: 16,
+    name: '',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-20230805-224051-jpg.jpg',
+  },
+  {
+    id: 17,
+    name: '',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-20230805-223639-jpg.jpg',
+  },
+  {
+    id: 18,
+    name: '',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-20230902-190002-jpg.jpg',
+  },
+  {
+    id: 19,
+    name: '',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-IMG-20230717-WA0065-jpg.jpg',
+  },
+  {
+    id: 20,
+    name: '',
+    category: 'Plats principaux',
+    image: 'NGALAM-TRAITEUR-IMG-20230717-WA0067-jpg.jpg',
+  }
 ];
